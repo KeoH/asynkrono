60b3e0c791c4fa37df7964223414f51d458790ed
Francisco Manzano <keoh77@gmail.com>
2018-01-22 13:07:57 +0100

    refactor

--- main.py
+++ main.py
@@ -3,39 +3,7 @@
 '''
 
 from aiohttp import web
-import aiohttp_jinja2, jinja2
-from db import db
-from views import setup_views
-import asyncio, websockets
-from settings import BASE_DIR, STATIC_ROOT, TEMPLATES_ROOT
-
-
-app = web.Application()
-
-# template engine config
-aiohttp_jinja2.setup(
-    app, loader = jinja2.FileSystemLoader(TEMPLATES_ROOT)
-)
-
-# view routing
-setup_views(app)
-
-#static files config
-app.router.add_static('/static/',
-    path = STATIC_ROOT,
-    name = 'static')
-
+from settings import app
 
 # starts the server
-web.run_app(app, host='127.0.0.1', port=8080)
-
-# El servidor web es bloqueante y no se ejecuta los websocket
-
-# async def echo(websocket, path):
-#     async for message in websocket:
-#         await websocket.send(message)
-
-# asyncio.get_event_loop().run_until_complete(
-#     websockets.serve(echo, 'localhost', 8765)
-# )
-# asyncio.get_event_loop().run_forever()
+web.run_app(app, host='0.0.0.0', port=8080)
--- settings.py
+++ settings.py
@@ -1,4 +1,8 @@
 import os
+from db import db
+import aiohttp_jinja2, jinja2
+from views import setup_views
+from aiohttp import web
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -6,3 +10,24 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 TEMPLATES_ROOT = os.path.join(BASE_DIR, 'templates')
 
 PAGINATION = 10
+
+MIDDLEWARES = [
+
+]
+
+app = web.Application(
+    middlewares = MIDDLEWARES
+)
+
+# template engine config
+aiohttp_jinja2.setup(
+    app, loader = jinja2.FileSystemLoader(TEMPLATES_ROOT)
+)
+
+# view routing
+setup_views(app)
+
+#static files config
+app.router.add_static('/static/',
+    path = STATIC_ROOT,
+    name = 'static')
