04e474c28eb62a6f7cf8f12cb9fbcd3c11afbacc
Francisco Manzano <keoh77@gmail.com>
2018-01-22 16:07:28 +0100

    fixed scrap

--- main.py
+++ main.py
@@ -3,7 +3,26 @@
 '''
 
 from aiohttp import web
-from settings import app
+
+import aiohttp_jinja2, jinja2
+from db import db
+from views import setup_views
+from settings import STATIC_ROOT, TEMPLATES_ROOT
+
+app = web.Application()
+
+# template engine config
+aiohttp_jinja2.setup(
+    app, loader = jinja2.FileSystemLoader(TEMPLATES_ROOT)
+)
+
+# view routing
+setup_views(app)
+
+#static files config
+app.router.add_static('/static/',
+    path = STATIC_ROOT,
+    name = 'static')
 
 # starts the server
 web.run_app(app, host='0.0.0.0', port=8080)
--- settings.py
+++ settings.py
@@ -1,12 +1,10 @@
 import os
 from db import db
-import aiohttp_jinja2, jinja2
-from views import setup_views
-from aiohttp import web
+
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 TEMPLATES_ROOT = os.path.join(BASE_DIR, 'templates')
 
-PAGINATION = 20
+PAGINATION = 10
